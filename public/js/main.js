fe8e4ed6f030692c4ceab3902f78a260049f40eb
Maxim Lakin <maximlakin@Maxims-MacBook-Air.local>
2013-11-29 22:20:48 -0500

    update

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,27 @@ $(function(){
    window.location = "post.html?pid="+$(this).attr('id');
    return false;
   });
+
+  $(".soc_open").hide();
+  $(".soc_close").click(function(e){
+    e.preventDefault();
+    $(".soc_close").hide();
+    $('.soc_share ul').animate({'opacity':'0','left':'-20px'},500,'linear',function(){
+      $(".soc_open").show();
+    });
+  });
+  $(".soc_open").click(function(e){
+    e.preventDefault();
+    $(".soc_open").hide();
+    $('.soc_share ul').animate({'opacity':'1','left':'0'},500,'linear',function(){
+      $(".soc_close").show();
+    });
+  });
+
+  $('.soc_share').hover(function(){
+    if($('.soc_share ul').css('opacity')==1){
+    $(".soc_close").show();
+  }
+  }, function(){$(".soc_close").hide();});
+
 })
